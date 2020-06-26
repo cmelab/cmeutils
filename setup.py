a7f525b964348a5e717ecee336f07ece46bdc03d
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-06-26 12:14:44 -0600

    cme_lab_utils not gixstapose XD

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='cme_lab_utils',
       author_email='ericjankowski@boisestate.edu',
       license='GPLv3',
       packages=['cme_lab_utils'],
+      package_dir={'cme_lab_utils': 'cme_lab_utils'},
       zip_safe=False,
       )
