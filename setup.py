f398844b4ba6d66a8162729adf12ad20f0541c9c
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-06-26 12:55:04 -0600

    playing around with setup --not sure why it doesn't work like I expect

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='cme_lab_utils',
-      version='0.0',
-      description='Helpful functions used in the CME lab',
-      url='https://gitlab.com/bsu/cme-lab/cme_lab_utils',
-      author='CME Lab',
-      author_email='ericjankowski@boisestate.edu',
-      license='GPLv3',
-      packages=['cme_lab_utils'],
-      package_dir={'cme_lab_utils': 'cme_lab_utils'},
-      zip_safe=False,
-      )
+setup(
+        name='cme_lab_utils',
+        version='0.0',
+        description='Helpful functions used in the CME lab',
+        url='https://gitlab.com/bsu/cme-lab/cme_lab_utils',
+        author='CME Lab',
+        author_email='ericjankowski@boisestate.edu',
+        license='GPLv3',
+        packages=find_packages(),
+        package_dir={'cme_lab_utils': 'cme_lab_utils'},
+        zip_safe=False,
+        )
