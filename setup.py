f3374ff5a57b7ae9e5823a72f3f3d0353f3cad71
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-06-26 10:12:26 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='cme_lab_utils',
+      version='0.0',
+      description='Helpful functions used in the CME lab'
+      url='https://gitlab.com/bsu/cme-lab/cme_lab_utils',
+      author='CME Lab',
+      author_email='ericjankowski@boisestate.edu',
+      license='GPLv3',
+      packages=['cme_lab_utils'],
+      zip_safe=False,
+      )
