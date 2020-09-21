48e486ae940ac6a29a745d21da06cc3ecacdf8a8
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2020-09-21 12:20:46 -0600

    oops, accidentally deleted function

--- cme_lab_utils/gsd_utils.py
+++ cme_lab_utils/gsd_utils.py
@@ -6,6 +6,34 @@ from scipy.sparse import csr_matrix
 from scipy.sparse.csgraph import connected_components
 
 
+def frame_get_type_position(gsdfile, typename, frame=-1):
+    """
+    This function returns the  positions of a particular particle
+    type from a frame of a gsd trajectory file.
+
+    Parameters
+    ----------
+    gsdfile : str,
+              filename of the gsd trajectory
+    typename : str,
+               name of particles of which to get the positions
+               (found in gsd.hoomd.Snapshot.particles.types)
+    frame : int,
+            frame number to get positions from. Supports
+            negative indexing. (default -1)
+
+    Returns
+    -------
+    numpy.ndarray
+    """
+    with gsd.hoomd.open(name=gsdfile, mode='rb') as f:
+        snap = f[frame]
+    typepos = snap.particles.position[
+            snap.particles.typeid == snap.particles.types.index(typename)
+            ]
+    return typepos
+
+
 def snap_bond_graph(snap):
     """
     Given a snapshot from a trajectory return an array
