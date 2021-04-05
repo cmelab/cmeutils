58aed628726d27b0799f548ac642ceeddca9cf77
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-05 12:16:46 -0600

    fix name/url in setup

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setup(name='cme_lab_utils',
+setup(name='cmeutils',
       version='0.0',
       description='Helpful functions used in the CME lab'
-      url='https://gitlab.com/bsu/cme-lab/cme_lab_utils',
+      url='https://github.com/cmelab/cmeutils',
       author='CME Lab',
       author_email='ericjankowski@boisestate.edu',
       license='GPLv3',
-      packages=['cme_lab_utils'],
+      packages=['cmeutils'],
       zip_safe=False,
       )
