4d2c8eb5975092cd0349aa71f097fa023e284702
Chris <chrisjones4@u.boisestate.edu>
2021-04-13 14:47:20 -0600

    updated test

--- cmeutils/tests/test_gsd.py
+++ cmeutils/tests/test_gsd.py
@@ -5,8 +5,9 @@ from base_test import BaseTest
 
 
 class TestGSD(BaseTest):
-    def test_frame_get_type_position(self, test_gsd):
-        pos_array = gsd_utils.frame_get_type_position(test_gsd, 'A')
+    def test_get_type_position(self, test_gsd):
+        pos_array = gsd_utils.get_type_position(gsd_file = test_gsd,
+                                type_name = 'A')
         assert type(pos_array) is type(np.array([]))
         assert pos_array.shape == (2,3)
 
