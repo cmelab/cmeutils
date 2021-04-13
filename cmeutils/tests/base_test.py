a810fc16bf278fdc8c32e92954250edbef0edcc0
Chris <chrisjones4@u.boisestate.edu>
2021-04-13 17:13:56 -0600

    add fixture for gsd file with bonds

--- cmeutils/tests/base_test.py
+++ cmeutils/tests/base_test.py
@@ -13,6 +13,11 @@ class BaseTest:
         create_gsd(filename)
         return filename
 
+    def test_gsd_with_bonds(self, tmp_path):
+        filename = tmp_path / "test.gsd"
+        create_gsd(filename, add_bonds=True)
+        return filename
+
     @pytest.fixture
     def test_snap(self, test_gsd):
         with gsd.hoomd.open(name=test_gsd, mode="rb") as f:
@@ -20,7 +25,7 @@ class BaseTest:
         return snap
 
 
-def create_frame(i, seed=42):
+def create_frame(i, add_bonds, seed=42):
     np.random.seed(seed)
     s = gsd.hoomd.Snapshot()
     s.configuration.step = i
@@ -29,8 +34,13 @@ def create_frame(i, seed=42):
     s.particles.typeid = [0,0,1,1]
     s.particles.position = np.random.random(size=(4,3))
     s.configuration.box = [3, 3, 3, 0, 0, 0]
+    if add_bonds:
+        s.bonds.N = 2
+        s.bonds.types = ['AA', 'BB']
+        s.bonds.typeid = [0, 1]
+        s.bonds.group = [[0, 1], [2, 3]]
     return s
 
-def create_gsd(filename):
+def create_gsd(filename, add_bonds=False):
     with gsd.hoomd.open(name=filename, mode='wb') as f:
-        f.extend((create_frame(i) for i in range(10)))
+        f.extend((create_frame(i, add_bonds) for i in range(10)))
