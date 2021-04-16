680f6e395721371bebd0074b6a21e87b93927a4f
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 13:18:14 -0600

    use new fixture names and assert clusteridx is correct

--- cmeutils/tests/test_gsd.py
+++ cmeutils/tests/test_gsd.py
@@ -1,32 +1,38 @@
 import numpy as np
 import pytest
 
-from cmeutils import gsd_utils
 from base_test import BaseTest
 
 
 class TestGSD(BaseTest):
-    def test_get_type_position(self, test_gsd):
+    def test_get_type_position(self, gsdfile):
         from cmeutils.gsd_utils import get_type_position
 
-        pos_array = get_type_position(gsd_file=test_gsd, typename="A")
+        pos_array = get_type_position(gsd_file=gsdfile, typename="A")
         assert type(pos_array) is type(np.array([]))
         assert pos_array.shape == (2, 3)
 
-    def test_validate_inputs(self, test_gsd, test_snap):
-        # Catch error with both gsd_file and snap are passed
-        with pytest.raises(ValueError):
-            snap = gsd_utils._validate_inputs(test_gsd, test_snap, 1)
-        with pytest.raises(AssertionError):
-            snap = gsd_utils._validate_inputs(test_gsd, None, 1.0)
-        with pytest.raises(AssertionError):
-            snap = gsd_utils._validate_inputs(None, test_gsd, 1)
-        with pytest.raises(OSError):
-            gsd_utils._validate_inputs("bad_gsd_file", None, 0)
+    def test_validate_inputs(self, gsdfile, snap):
+        from cmeutils.gsd_utils import _validate_inputs
 
-    def test_get_all_types(self, test_gsd):
-        types = gsd_utils.get_all_types(test_gsd)
+        # Catch errors when both gsd_file and snap are passed
+        with pytest.raises(ValueError):
+            _validate_inputs(gsdfile, snap, 1)
+        with pytest.raises(AssertionError):
+            _validate_inputs(gsdfile, None, 1.0)
+        with pytest.raises(AssertionError):
+            _validate_inputs(None, gsdfile, 1)
+        with pytest.raises(OSError):
+            _validate_inputs("bad_gsd_file", None, 0)
+
+    def test_get_all_types(self, gsdfile):
+        from cmeutils.gsd_utils import get_all_types
+
+        types = get_all_types(gsdfile)
         assert types == ["A", "B"]
 
-    def test_snap_molecule_cluster(self, test_gsd_bonded):
-        cluster = gsd_utils.snap_molecule_cluster(gsd_file=test_gsd_bonded)
+    def test_snap_molecule_cluster(self, gsdfile_bond):
+        from cmeutils.gsd_utils import snap_molecule_cluster
+
+        cluster = snap_molecule_cluster(gsd_file=gsdfile_bond)
+        assert np.array_equal(cluster, [0, 1, 0, 1, 2])
