96c2c35d05049f59f9fa9e96bee8cdd1b057d04c
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 13:20:53 -0600

    I forgot to change it so it was AA :woman-facepalming:

--- cmeutils/tests/test_structure.py
+++ cmeutils/tests/test_structure.py
@@ -15,7 +15,7 @@ class TestStructure(BaseTest):
     def test_gsd_rdf_samename(self, gsdfile_bond):
         from cmeutils.structure import gsd_rdf
 
-        rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "B")
-        rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "B", exclude_bonded=False)
+        rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "A")
+        rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "A", exclude_bonded=False)
         assert norm2 == 1
         assert rdf_noex != rdf_ex
