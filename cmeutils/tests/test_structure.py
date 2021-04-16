cdbd222e7ef155810283ee238f59307fdac5b902
chrisjonesBSU <chrisjones4@u.bosiestate.edu>
2021-04-16 12:51:14 -0600

    one more assertion added

--- cmeutils/tests/test_structure.py
+++ cmeutils/tests/test_structure.py
@@ -7,5 +7,6 @@ class TestStructure(BaseTest):
     def test_gsd_rdf(self, test_gsd_bonded):
         rdf_exclude, norm = structure.gsd_rdf(test_gsd_bonded, "A", "B")
         rdf_dont_exclude, norm2 = structure.gsd_rdf(test_gsd_bonded, "A", "B", exclude_bonded=False)
+        assert norm2 == 1
         assert rdf_dont_exclude != rdf_exclude
 
