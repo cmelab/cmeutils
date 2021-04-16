f48b6eea9f168b3ce99810c525027222ac288141
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-04-16 13:17:47 -0600

    add test for aa

--- cmeutils/tests/test_structure.py
+++ cmeutils/tests/test_structure.py
@@ -1,15 +1,21 @@
 import pytest
 
 from cmeutils.tests.base_test import BaseTest
-from cmeutils import structure
 
 
 class TestStructure(BaseTest):
-    def test_gsd_rdf(self, test_gsd_bonded):
-        rdf_exclude, norm = structure.gsd_rdf(test_gsd_bonded, "A", "B")
-        rdf_dont_exclude, norm2 = structure.gsd_rdf(
-            test_gsd_bonded, "A", "B", exclude_bonded=False
-        )
-        assert norm2 == 1
-        assert rdf_dont_exclude != rdf_exclude
+    def test_gsd_rdf(self, gsdfile_bond):
+        from cmeutils.structure import gsd_rdf
 
+        rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "B")
+        rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "B", exclude_bonded=False)
+        assert norm2 == 1
+        assert rdf_noex != rdf_ex
+
+    def test_gsd_rdf_samename(self, gsdfile_bond):
+        from cmeutils.structure import gsd_rdf
+
+        rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "B")
+        rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "B", exclude_bonded=False)
+        assert norm2 == 1
+        assert rdf_noex != rdf_ex
