b50b4c0e98b4d9db5ac5b7d3d6354455278fffb2
chrisjonesbsu <chrisjones4@u.boisestate.edu>
2021-05-10 21:23:59 -0600

    added some assertions

--- cmeutils/polymers.py
+++ cmeutils/polymers.py
@@ -12,18 +12,21 @@ class System:
         self.clusters = snap.molecule_cluster(gsd_file, snap, gsd_frame)
         self.molecule_ids = set(self.clusters)
         self.n_compounds = len(self.molecule_ids)
-        self.n_atoms = len(self.molecule_ids)
-        self.molecules = None
+        self.n_atoms = len(self.clusters)
+        self.n_monomers = int(self.n_compounds / self.atoms_per_monomer)
+        assert self.n_atoms == (self.n_monomers * self.atoms_per_monomer)
+        self.molecules = self.generate_molecules() 
         self.monomers = None
         self.segments = None
 
     def generate_molecules(self):
-        self.molecules = [Molecule(self, i) for i in self.molecule_ids]
+        molecules = [Molecule(self, i) for i in self.molecule_ids]
+        return molecules
 
     def generate_monomers(self):
         # Call the monomers func in Molecule instead?
-        self.monomers = [Monomer(mol, monomers_per_molecule)
-                for mol in self.molecules]
+        monomers = [Monomer(self, mol)for mol in self.molecules]
+        return monomers
 
 
     def generate_segments(self,
@@ -49,16 +52,19 @@ class Molecule:
     """
     def __init__(self, system, molecule_id):
         self.system = system
-        self.molecule_id = molecule_id
+        self.molecule_id = molecule_id # Need this?
         self.atom_indices = np.where(system.clusters == self.molecule_id)[0]
         self.n_atoms = len(self.atom_indices)
         self.monomers = None
         self.segments = None
 
-    def monomers(self, monomers_per_molecule):
+    def monomers(self):
+        atoms_per_monomer = self.system.atoms_per_monomer
+        molecule_length = int(self.n_atoms / atoms_per_monomer)
         monomer_indices = np.array_split(self.atom_indices,
-                monomers_per_molecule
+                molecule_length
                 )
+        assert len(monomer_indices) == molecule_length
         self.monomers = [Monomer(self, i) for i in monomer_indices]
 
     def segments(self, segments_per_molecule):
@@ -84,9 +90,10 @@ class Molecule:
 class Monomer:
     def __init__(self, molecule, atom_indices):
         self.molecule = molecule
+        self.system = molecule.system
         self.atom_indices = atom_indices
         self.n_atoms = len(self.atom_indices)
-        
+        assert self.n_atoms == self.system.atoms_per_monomer 
 
     def center_of_mass(self):
         pass
@@ -95,6 +102,7 @@ class Monomer:
 class Segment:
     def __init__(self, molecule, atom_indices):
         self.molecule = molecule
+        self.system = molecule.system
         self.atom_indices = atom_indices
         self.n_atoms = len(self.atom_indices)
 
@@ -103,4 +111,6 @@ class Segment:
 
     def end_to_end_distance(self):
         pass
-
+   
+   def bond_vectors(self):
+       pass
