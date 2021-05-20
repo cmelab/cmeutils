4bffb6191df80d0b3b3193be78c3491a2f6a7241
Chris <chrisjones4@u.boisestate.edu>
2021-05-20 16:49:38 -0600

    add angles to snapshot writer

--- cmeutils/writers.py
+++ cmeutils/writers.py
@@ -13,6 +13,7 @@ def write_snapshot(beads):
     all_pairs = []
     pair_groups = []
     all_angles = []
+    angle_groups = []
     all_pos = []
     box = None
 
@@ -26,25 +27,41 @@ def write_snapshot(beads):
                 pair = sorted([bead.name, beads[idx+1].name])
                 all_pairs.append(f"{pair[0]}-{pair[1]}")
                 pair_groups.append([idx, idx+1])
-        except:
+
+                if bead.parent == beads[idx+2].parent:
+                    angle = [bead.name, beads[idx+1].name, beads[idx+2].name]
+                    all_angles.append(
+                            f"{angle[0]}-{angle[1]}-{angle[2]}"
+                            )
+                    angle_groups.append([idx, idx+1, idx+2])
+        except IndexError:
             pass
 
     types = list(set(all_types)) 
     pairs = list(set(all_pairs)) 
+    angles = list(set(all_angles))
     type_ids = [np.where(np.array(types)==i)[0][0] for i in all_types]
     pair_ids = [np.where(np.array(pairs)==i)[0][0] for i in all_pairs]
+    angle_ids = [np.where(np.array(angles)==i)[0][0] for i in all_angles]
 
     s = gsd.hoomd.Snapshot()
+    #Particles
     s.particles.N = len(all_types)
-    s.bonds.N = len(all_pairs)
-    s.bonds.M = 2
-    s.angles.N = len(all_angles)
     s.particles.types = types 
     s.particles.typeid = np.array(type_ids) 
     s.particles.position = np.array(all_pos)
+    #Bonds
+    s.bonds.N = len(all_pairs)
+    s.bonds.M = 2
     s.bonds.types = pairs
     s.bonds.typeid = np.array(pair_ids)
     s.bonds.group = np.vstack(pair_groups)
+    #Angles
+    s.angles.N = len(all_angles)
+    s.angles.M = 3
+    s.angles.types = angles
+    s.angles.typeid = np.array(angle_ids)
+    s.angles.group = np.vstack(angle_groups)
     s.configuration.box = box 
     return s
 
