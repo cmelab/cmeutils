8798558f90bfa8b593cc42276a366bec2708c625
Chris <chrisjones4@u.boisestate.edu>
2021-05-24 15:48:47 -0600

    can use components to find bond length and angle distributions

--- cmeutils/polymers.py
+++ cmeutils/polymers.py
@@ -113,6 +113,7 @@ class System:
             normalize=False,
             use_monomers=True,
             use_segments=False,
+            use_components=False,
             nbins='auto',
             plot=False
             ):
@@ -124,6 +125,7 @@ class System:
                     [np.linalg.norm(vec) for vec in molecule.bond_vectors(
                         use_monomers=use_monomers,
                         use_segments=use_segments,
+                        use_components=use_components,
                         normalize=normalize,
                         )
                         ]
@@ -136,6 +138,7 @@ class System:
             self,
             use_monomers=True,
             use_segments=False,
+            use_components=False,
             nbins='auto',
             plot=False
             ):
@@ -145,7 +148,8 @@ class System:
         for molecule in self.molecules:
             bond_angles.extend(molecule.bond_angles(
                 use_monomers=use_monomers,
-                use_segments=use_segments
+                use_segments=use_segments,
+                use_components=use_components
                     )
                 )
 
@@ -275,14 +279,15 @@ class Molecule(Structure):
                 )
         self.monomers = self.generate_monomers() 
         self.n_monomers = len(self.monomers)
-        self.segments = None
-        self.n_segments = None
+        self.segments = [] 
+        self.components = []
         self.sequence = None
 
-    def assign_types(self, sequence):
-        n = self.n_monomers // len(sequence)
-        monomer_sequence = sequence * n
-        monomer_sequence += sequence[:(self.n_monomers - len(monomer_sequence))]
+    def assign_types(self):
+        n = self.n_monomers // len(self.sequence)
+        monomer_sequence = self.sequence * n
+        monomer_sequence += self.sequence[:(self.n_monomers
+            - len(monomer_sequence))]
         for i, name in enumerate(list(monomer_sequence)):
             self.monomers[i].name = name
 
@@ -307,8 +312,7 @@ class Molecule(Structure):
                 self.atom_indices,
                 segments_per_molecule
                 )
-        self.segments = [Segment(self, i) for i in segment_indices]
-        self.n_segments = len(self.segments)
+        self.segments.extend([Segment(self, i) for i in segment_indices])
     
     def end_to_end_distance(self, squared=False):
         """Retruns the magnitude of the vector connecting the first and
@@ -341,6 +345,7 @@ class Molecule(Structure):
             self,
             use_monomers=True,
             use_segments=False,
+            use_components=False,
             normalize=False
             ):
         """Generates a list of the vectors connecting subsequent monomer 
@@ -365,7 +370,11 @@ class Molecule(Structure):
         list of numpy.ndarray, shape=(3,), dtype=float
 
         """
-        sub_structures = self._sub_structures(use_monomers, use_segments)
+        sub_structures = self._sub_structures(
+                use_monomers,
+                use_segments,
+                use_components
+                )
 
         vectors = []
         for idx, structure in enumerate(sub_structures):
@@ -388,7 +397,8 @@ class Molecule(Structure):
             self,
             bond_vector_list=None,
             use_monomers=True,
-            use_segments=False):
+            use_segments=False,
+            use_components=False):
         """Generates a list of the angles between subsequent monomer 
         or segment bond vectors.
 
@@ -415,7 +425,10 @@ class Molecule(Structure):
 
         """
         if bond_vector_list is None:
-            bond_vector_list = self.bond_vectors(use_monomers, use_segments)
+            bond_vector_list = self.bond_vectors(
+                    use_monomers,
+                    use_segments,
+                    use_components)
 
         angles = []
         for idx, vector in enumerate(bond_vector_list):
@@ -437,24 +450,24 @@ class Molecule(Structure):
         ""
         pass
 
-    def _sub_structures(self, use_monomers, use_segments):
+    def _sub_structures(self, monomers, segments, components):
         """
         """
-        if all((use_monomers, use_segments)):
+        if all((monomers, segments, components)):
             raise ValueError(
-                    "Only one of `use_monomers` and `use_segments` "
+                    "Only one of `monomers`, `segments`, and `components` "
                     "can be set to `True`"
                     )
-        if not any((use_monomers, use_segments)):
+        if not any((monomers, segments, components)):
             raise ValueError(
-                    "Set one of `use_monomers` or `use_segments` to "
+                    "Set one of `monomers`, `segments`, `components` to "
                     "`True` depending on which structure bond vectors "
                     "you want returned."
                     )
 
-        if use_monomers:
+        if monomers:
             sub_structures = self.monomers
-        elif use_segments:
+        elif segments:
             if self.segments == None:
                 raise ValueError(
                         "The segments for this molecule have not been "
@@ -462,6 +475,14 @@ class Molecule(Structure):
                         "the `Molecule` class."
                         )
             sub_structures = self.segments
+        elif components:
+            if self.components == None:
+                raise ValueError(
+                        "The components for this molecule have not been "
+                        "created. See the `generate_components()` method for "
+                        "the `Monomer` class."
+                        )
+            sub_structures = self.components
         return sub_structures
 
 
@@ -474,10 +495,12 @@ class Monomer(Structure):
                 parent=parent,
                 atom_indices=atom_indices
                 )
-        self.components = None
+        self.components = [] 
         assert self.n_atoms == self.system.atoms_per_monomer 
         
     def generate_components(self, index_mapping):
+        if self.components:
+            raise ValueError("Components have already been generated")
         components = []
         for name, indices in index_mapping.items():
             if all([isinstance(i, list) for i in indices]):
@@ -495,7 +518,8 @@ class Monomer(Structure):
                         atom_indices = self.atom_indices[indices]
                         )
                 components.append(component)
-        self.components = components
+        self.components.extend(components)
+        self.parent.components.extend(components)
 
 
 class Segment(Structure):
