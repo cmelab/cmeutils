c4c4b1a9ade7caab8da42d32181f64ad21e3557a
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-06-15 17:07:27 -0600

    add very basic test for get quaternions and switch to np.array_equal

--- cmeutils/tests/test_structure.py
+++ cmeutils/tests/test_structure.py
@@ -1,5 +1,7 @@
 import pytest
 
+import numpy as np
+
 from cmeutils.tests.base_test import BaseTest
 
 
@@ -10,7 +12,7 @@ class TestStructure(BaseTest):
         rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "B")
         rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "B", exclude_bonded=False)
         assert norm2 == 1
-        assert rdf_noex != rdf_ex
+        assert not np.array_equal(rdf_noex, rdf_ex)
 
     def test_gsd_rdf_samename(self, gsdfile_bond):
         from cmeutils.structure import gsd_rdf
@@ -18,4 +20,17 @@ class TestStructure(BaseTest):
         rdf_ex, norm = gsd_rdf(gsdfile_bond, "A", "A")
         rdf_noex, norm2 = gsd_rdf(gsdfile_bond, "A", "A", exclude_bonded=False)
         assert norm2 == 1
-        assert rdf_noex != rdf_ex
+        assert not np.array_equal(rdf_noex, rdf_ex)
+
+    def test_get_quaternions(self):
+        from cmeutils.structure import get_quaternions
+
+        with pytest.raises(ValueError):
+            get_quaternions(0)
+
+        with pytest.raises(ValueError):
+            get_quaternions(5.3)
+
+        qs = get_quaternions()
+        assert len(qs) == 20
+        assert len(qs[0]) == 4
