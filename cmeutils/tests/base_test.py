2ee81b6cffc1b7919980829ea7a64905ed7d2d16
chrisjonesBSU <chrisjones4@u.boisestate.edu>
2021-08-10 14:26:59 -0600

    option to write images to a gsd file, test to catch msd warning for no images

--- cmeutils/tests/base_test.py
+++ cmeutils/tests/base_test.py
@@ -20,13 +20,19 @@ class BaseTest:
         return filename
 
     @pytest.fixture
+    def gsdfile_images(self, tmp_path):
+        filename = tmp_path / "test_images.gsd"
+        create_gsd(filename, images=True)
+        return filename
+
+    @pytest.fixture
     def snap(self, gsdfile):
         with gsd.hoomd.open(name=gsdfile, mode="rb") as f:
             snap = f[-1]
         return snap
 
 
-def create_frame(i, add_bonds, seed=42):
+def create_frame(i, add_bonds, images, seed=42):
     np.random.seed(seed)
     s = gsd.hoomd.Snapshot()
     s.configuration.step = i
@@ -40,10 +46,14 @@ def create_frame(i, add_bonds, seed=42):
         s.bonds.types = ["AB"]
         s.bonds.typeid = [0, 0]
         s.bonds.group = [[0, 2], [1, 3]]
+    if images:
+        s.particles.image = np.full(shape=(5,3), fill_value=i)
+    else:
+        s.particles.image = np.zeros(shape=(5,3))
     s.validate()
     return s
 
 
-def create_gsd(filename, add_bonds=False):
+def create_gsd(filename, add_bonds=False, images=False):
     with gsd.hoomd.open(name=filename, mode="wb") as f:
-        f.extend((create_frame(i, add_bonds=add_bonds) for i in range(10)))
+        f.extend((create_frame(i, add_bonds=add_bonds, images=images) for i in range(10)))
