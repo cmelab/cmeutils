a68297d0fa75158a057c641325c66d2ef48d2b0d
Gwen White <gwenwhite@u.boisestate.edu>
2021-08-27 18:05:15 -0600

    Made a new function that will plot rdf of all atom types. It is called on in gsd_rdf when A_name or B_name is None.

--- cmeutils/structure.py
+++ cmeutils/structure.py
@@ -4,6 +4,82 @@ import gsd
 import gsd.hoomd
 import numpy as np
 
+def all_atom_rdf(gsdfile,
+                 start=0,
+                 stop=None,
+                 r_max=None,
+                 r_min=0,
+                 bins=100,
+                 exclude_bonded=True):
+    """ Parameters
+    ----------
+    gsdfile : str
+        Filename of the GSD trajectory.
+    A_name, B_name : str
+        Name(s) of particles between which to calculate the RDF (found in
+        gsd.hoomd.Snapshot.particles.types)
+    start : int
+        Starting frame index for accumulating the RDF. Negative numbers index
+        from the end. (default 0)
+    stop : int
+        Final frame index for accumulating the RDF. If None, the last frame
+        will be used. (default None)
+    r_max : float
+        Maximum radius of RDF. If None, half of the maximum box size is used.
+        (default None)
+    r_min : float
+        Minimum radius of RDF. (default 0)
+    bins : int
+        Number of bins to use when calculating the RDF. (default 100)
+    exclude_bonded : bool
+        Whether to remove particles in same molecule from the neighbor list.
+        (default True)
+
+    Returns
+    -------
+    (freud.density.RDF, float)
+    """
+
+
+    if not stop:
+        stop = -1
+
+    with gsd.hoomd.open(gsdfile, mode="rb") as trajectory:
+        snap = trajectory[0]
+
+        if r_max is None:
+           #Use a value just less than half the maximum box length.
+            r_max = np.nextafter(
+            np.max(snap.configuration.box[:3]) * 0.5, 0, dtype=np.float32
+            )
+
+        rdf = freud.density.RDF(bins=bins, r_max=r_max, r_min=r_min)
+        if exclude_bonded:
+            exclude_ii = True
+            molecules = gsd_utils.snap_molecule_cluster(snap=snap)
+            
+        for snap in trajectory[start:stop]:
+            box = snap.configuration.box
+            points = snap.particles.position[:]
+            system = (box, points)
+            aq = freud.locality.AABBQuery.from_system(system)
+            nlist = aq.query(
+                points, {"r_max": r_max, "exclude_ii": exclude_ii}
+            ).toNeighborList()
+
+            if exclude_bonded:
+                pre_filter = len(nlist)
+                nlist.filter(
+                    molecules[nlist.point_indices]
+                    != molecules[nlist.query_point_indices]
+                )
+                post_filter = len(nlist)
+
+            rdf.compute(aq, neighbors=nlist, reset=False)
+            
+        normalization = post_filter / pre_filter if exclude_bonded else 1
+        return rdf, normalization
+
 
 def gsd_rdf(
     gsdfile,
@@ -15,7 +91,7 @@ def gsd_rdf(
     r_min=0,
     bins=100,
     exclude_bonded=True,
-):
+    ):
     """Compute intermolecular RDF from a GSD file.
 
     This function calculates the radial distribution function given a GSD file
@@ -53,53 +129,50 @@ def gsd_rdf(
     -------
     (freud.density.RDF, float)
     """
-    if not stop:
-        stop = -1
+    if A_name == None or B_name == None:
+        return all_atom_rdf(gsdfile, start, stop, r_max, r_min,bins, exclude_bonded) 
+    else:
+        if not stop:
+            stop = -1
+        with gsd.hoomd.open(gsdfile, mode='rb') as trajectory:
+            snap = trajectory[0]
+        
+            if r_max is None:
+                #Use a value just less than half the maximum box length.
+                r_max = np.nextafter(np.max(snap.configuration.box[:3]) * 0.5, 0, dtype=np.float32)
+            rdf = freud.density.RDF(bins=bins, r_max=r_max, r_min=r_min)
 
-    with gsd.hoomd.open(gsdfile, mode="rb") as trajectory:
-        snap = trajectory[0]
-
-        if r_max is None:
-            # Use a value just less than half the maximum box length.
-            r_max = np.nextafter(
-                np.max(snap.configuration.box[:3]) * 0.5, 0, dtype=np.float32
-            )
-
-        rdf = freud.density.RDF(bins=bins, r_max=r_max, r_min=r_min)
-
-        type_A = snap.particles.typeid == snap.particles.types.index(A_name)
-        type_B = snap.particles.typeid == snap.particles.types.index(B_name)
-
-        if exclude_bonded:
-            molecules = gsd_utils.snap_molecule_cluster(snap=snap)
-            molecules_A = molecules[type_A]
-            molecules_B = molecules[type_B]
-
-        for snap in trajectory[start:stop]:
-            A_pos = snap.particles.position[type_A]
-            if A_name == B_name:
-                B_pos = A_pos
-                exclude_ii = True
-            else:
-                B_pos = snap.particles.position[type_B]
-                exclude_ii = False
-
-            box = snap.configuration.box
-            system = (box, A_pos)
-            aq = freud.locality.AABBQuery.from_system(system)
-            nlist = aq.query(
-                B_pos, {"r_max": r_max, "exclude_ii": exclude_ii}
-            ).toNeighborList()
-
+            type_A = snap.particles.typeid == snap.particles.types.index(A_name)
+            type_B = snap.particles.typeid == snap.particles.types.index(B_name)
+    
             if exclude_bonded:
-                pre_filter = len(nlist)
-                nlist.filter(
-                    molecules_A[nlist.point_indices]
-                    != molecules_B[nlist.query_point_indices]
-                )
-                post_filter = len(nlist)
+                molecules = gsd_utils.snap_molecules_cluster(snap=snap)
+                molecules_A = molecules[type_A]
+                molecules_B = molecules[type_B]
+            for snap in trajectory[start:stop]:
+                A_pos = snap.particles.position[type_A]
+                if A_name == B_name:
+                    B_pos = A_pos
+                    exclude_ii = True
+                else:
+                    B_pos = snap.particles.position[type_B]
+                    exclude_ii = False
 
-            rdf.compute(aq, neighbors=nlist, reset=False)
+                box = snap.configuration.box
+                system = (box, A_pos)
+                aq = freud.locality.AABBQuery.from_system(system)
+                nlist = aq.query(
+                    B_pos, {"r_max":r_max, "exclude_ii":exclude_ii}
+                ).toNeighborList()
 
-        normalization = post_filter / pre_filter if exclude_bonded else 1
-        return rdf, normalization
+                if exclude_bonded:
+                    pre_filter = len(nlist)
+                    nlist.filter(
+                        molecules_A[nlist.point.indices]
+                        != molecules_B[nlist.query_point_indices]
+                    )
+                    post_filter = len(nlist)
+
+                rdf.compute(aq, neighbors=nlist, reset=False)
+    normalization = post_filter / pre_filter if exclude_bonded else 1
+    return rdf, normalization    
