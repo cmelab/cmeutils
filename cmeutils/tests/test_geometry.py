359d479a571d84c1a60496f50d6c7c14be310573
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-10-19 11:02:52 -0600

    Add test skeleton

---
+++ cmeutils/tests/test_geometry.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pytest
+
+from base_test import BaseTest
+from cmeutils.geometry import get_plane_normal, angle_between_vectors
+
+
+class TestGeometry(BaseTest):
+    def test_get_plane_normal(self):
+        pass
+
+    def test_angle_between_vectors(self):
+        pass
