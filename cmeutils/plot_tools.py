11783aa04784d55fd44790329903e04482953b3f
Gwen <gwen@admins-mbp.boisestate.edu>
2022-01-04 12:18:10 -0700

    Added kwargs for peak height.

--- cmeutils/plot_tools.py
+++ cmeutils/plot_tools.py
@@ -1,21 +1,23 @@
 import scipy.signal as signal
 
 
-def find_peaks(data):
+def find_peaks(data, height=None):
     """Finds peaks in 1-D data.
-    The peaks are any value greater than the average of all the data values. The data values are 1-D values (like x, y, or z) that     you wish to find the peaks of. 
-    Any points/values that excede the max height will be identified as peaks.
+    The peaks are any value greater than the variable entered for height. The data values are 1-D values (like x, y, or z)   
+    that you wish to find the peaks of.
+    Any points/values that excede the height will be identified as peaks.
     
     Parameters
     ----------
     data : numpy.ndarray, shape (N,1)
        Such as x, y, OR z.
+    max_height : int, default None
+        Required height of peaks. 
+        
     Returns
     ----------
     tuple
         The indices of peaks
     """
-    total_peaks = signal.find_peaks(data)
-    avg_peaks = sum(data) / len(data)
-    peaks = signal.find_peaks(data, height=avg_peaks)
-    return peaks 
+    peaks = signal.find_peaks(data, height=height)
+    return peaks  
