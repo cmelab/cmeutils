8a9cc3f9d8058c4e2a56c0ea73c1c8dcea5209a1
chrisjonesbsu <chrisjones4@u.boisestate.edu>
2023-09-16 11:31:59 -0600

    add material and camera

--- cmeutils/visualize.py
+++ cmeutils/visualize.py
@@ -6,32 +6,90 @@ import numpy as np
 class FresnelGSD:
     def __init__(
             self,
-            material,
             gsd_file,
             frame=0,
             color_dict=None,
-            camera=fresnel.camera.Orthographic,
+            solid=0.1,
+            roughness=0.3,
+            specular=0.5,
+            specular_trans=0,
+            metal=0,
+            view_axis=(1, 0, 0)
     ):
+        self.scene = fresnel.Scene()
         self.gsd_file = gsd_file
-        self._scene = fresnel.Scene()
-        self.color_dict = color_dict
-        self._camera = camera
         self._snapshot = None
-        self._frame = None
+        self._frame = 0 
         self.frame = frame
-        self.material = fresnel.material.Material()
+        self._height = 5
+        self.color_dict = color_dict
+        # Set fresnel.material.Material attrs
+        self.solid = solid
+        self.roughness = roughness
+        self.specular = specular
+        self.specular_trans = specular_trans
+        self.metal = metal
+        # Set fresnel.camera attrs
+        self.view_axis = np.asarray(view_axis)
+        self._up = np.array([0, 0, 1])
 
     @property
     def geometry(self):
         geometry = fresnel.geometry.Sphere(
-            scene=self._scene,
+            scene=self.scene,
             position=self.positions,
             radius=self.radius,
             color=fresnel.color.linear(self.colors),
         )
         geometry.material = self.material
         return geometry
+
+    @property
+    def material(self):
+        material = fresnel.material.Material(
+                primitive_color_mix=1,
+                solid=self.solid,
+                roughness=self.roughness,
+                specular=self.specular,
+                spec_trans=self.specular_trans,
+                metal=self.metal
+        )
+        return material
+
+    @property
+    def camera(self):
+        camera=fresnel.camera.Orthographic(
+                position=self.camera_position,
+                look_at=self.look_at,
+                up=self.up,
+                height=self.height
+        )
+        return camera
+
+    @property
+    def camera_position(self):
+        return (self.snapshot.configuration.box[:3] * self.view_axis) - 0.5
+
+    @property
+    def look_at(self):
+        return self.snapshot.configuration.box[:3] * -self.view_axis 
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
+
+    @property
+    def up(self):
+        return self._up
     
+    @up.setter
+    def up(self, value):
+        self._up = value
+
     @property
     def snapshot(self):
         return self._snapshot
@@ -71,5 +129,7 @@ class FresnelGSD:
             return np.array([0.5, 0.25, 0.5])
 
     def view(self):
+        self.scene.camera = self.camera
+        self.scene.geometry = [self.geometry]
         fresnel.preview(self.scene)
 
