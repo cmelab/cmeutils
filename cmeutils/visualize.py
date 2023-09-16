e8bf10bcf3425b8f323942933e68d635e402ba0f
chrisjonesbsu <chrisjones4@u.boisestate.edu>
2023-09-16 12:44:57 -0600

    add a bunch of properties and setters

--- cmeutils/visualize.py
+++ cmeutils/visualize.py
@@ -9,29 +9,131 @@ class FresnelGSD:
             gsd_file,
             frame=0,
             color_dict=None,
+            diameter_scale=0.30,
             solid=0.1,
             roughness=0.3,
             specular=0.5,
             specular_trans=0,
             metal=0,
-            view_axis=(1, 0, 0)
-    ):
+            view_axis=(1, 0, 0),
+            height=10
+        ):
         self.scene = fresnel.Scene()
         self.gsd_file = gsd_file
         self._snapshot = None
         self._frame = 0 
         self.frame = frame
-        self._height = 5
+        self._height = height 
         self.color_dict = color_dict
+        self._diameter_scale = diameter_scale
         # Set fresnel.material.Material attrs
         self.solid = solid
-        self.roughness = roughness
-        self.specular = specular
-        self.specular_trans = specular_trans
-        self.metal = metal
+        self._roughness = roughness
+        self._specular = specular
+        self._specular_trans = specular_trans
+        self._metal = metal
         # Set fresnel.camera attrs
-        self.view_axis = np.asarray(view_axis)
+        self._view_axis = np.asarray(view_axis)
         self._up = np.array([0, 0, 1])
+        self.build_view()
+
+    @property
+    def frame(self):
+        return self._frame
+
+    @frame.setter
+    #TODO: Assert num frames
+    def frame(self, frame):
+        self._frame = frame
+        with gsd.hoomd.open(self.gsd_file) as f:
+            self._snapshot = f[frame]
+
+    @property
+    def snapshot(self):
+        return self._snapshot
+
+    @property
+    def diameter_scale(self):
+        return self._diameter_scale
+
+    @diameter_scale.setter
+    def diameter_scale(self, value):
+        self._diameter_scale = value
+
+    @property
+    def color_dict(self):
+        return self._color_dict
+
+    @color_dict.setter
+    def color_dict(self, value):
+        self._color_dict = value
+
+    def set_type_color(self, particle_type, color):
+        self._color_dict[particle_type] = color
+
+    @property
+    def solid(self):
+        return self._solid
+
+    @solid.setter
+    def solid(self, value):
+        self._solid = value
+
+    @property
+    def roughness(self):
+        return self._roughness
+
+    @roughness.setter
+    def roughness(self, value):
+        self._roughness = value
+
+    @property
+    def specular(self):
+        return self._specular
+
+    @specular.setter
+    def specular(self, value):
+        self._specular = value
+
+    @property
+    def specular_trans(self):
+        return self._specular_trans
+
+    @specular_trans.setter
+    def specular_trans(self, value):
+        self._specular_trans = value
+
+    @property
+    def metal(self):
+        return self._metal
+
+    @metal.setter
+    def metal(self, value):
+        self._metal = value
+
+    @property
+    def view_axis(self):
+        return self._view_axis
+
+    @view_axis.setter
+    def view_axis(self, value):
+        self._view_axis = np.asarray(value)
+
+    @property
+    def up(self):
+        return self._up
+
+    @up.setter
+    def up(self, value):
+        self._up = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        self._height = value
 
     @property
     def geometry(self):
@@ -74,36 +176,6 @@ class FresnelGSD:
     def look_at(self):
         return self.snapshot.configuration.box[:3] * -self.view_axis 
 
-    @property
-    def height(self):
-        return self._height
-
-    @height.setter
-    def height(self, value):
-        self._height = value
-
-    @property
-    def up(self):
-        return self._up
-    
-    @up.setter
-    def up(self, value):
-        self._up = value
-
-    @property
-    def snapshot(self):
-        return self._snapshot
-
-    @property
-    def frame(self):
-        return self._frame
-
-    @frame.setter
-    #TODO: Assert num frames
-    def frame(self, frame):
-        self._frame = frame
-        with gsd.hoomd.open(self.gsd_file) as f:
-            self._snapshot = f[frame]
 
     @property
     def positions(self):
@@ -111,12 +183,15 @@ class FresnelGSD:
 
     @property
     def radius(self):
-        return self.snapshot.particles.diameter / 2
+        return self.snapshot.particles.diameter * self.diameter_scale 
     
     @property
     def particle_types(self):
         return np.array(
-                [snapshot.particles.types[i] for i in snap.particles.typeid]
+                [
+                    self.snapshot.particles.types[i] for
+                    i in self.snapshot.particles.typeid
+                ]
         )
 
     @property
@@ -128,8 +203,6 @@ class FresnelGSD:
         else:
             return np.array([0.5, 0.25, 0.5])
 
-    def view(self):
+    def build_view(self):
         self.scene.camera = self.camera
         self.scene.geometry = [self.geometry]
-        fresnel.preview(self.scene)
-
