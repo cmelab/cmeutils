6b83bc50c53f920635021ee1ac86ee315b04ac39
Chris Jones <50423140+chrisjonesBSU@users.noreply.github.com>
2023-09-19 13:29:25 -0600

    Add setter for colors property (#58)
    * set a heigh multiplier
    
    * only use hidden height factor attr in default height method
    
    * fix camera position buffer
    
    * remove buffer from camera position
    
    * add unit test for camera pos
    
    * simplify colors property
    
    * add unit test for setting colors
    
    * test setter

--- cmeutils/tests/test_visualize.py
+++ cmeutils/tests/test_visualize.py
@@ -45,6 +45,17 @@ class TestFresnelGSD(BaseTest):
         )
         p3ht_fresnel.view()
 
+    def test_camera_position(self, p3ht_fresnel):
+        init_pos = p3ht_fresnel.camera_position
+        p3ht_fresnel.view_axis = (1, 1, 0)
+        assert not all(np.not_equal(init_pos, p3ht_fresnel.camera_position))
+        box = p3ht_fresnel.snapshot.configuration.box[:3]
+        assert np.array_equal(
+            p3ht_fresnel.camera_position, box * np.array([1, 1, 0])
+        )
+        camera = p3ht_fresnel.camera()
+        assert np.array_equal(camera.position, box * np.array([1, 1, 0]))
+
     def test_set_color_no_type(self, p3ht_fresnel):
         with pytest.raises(ValueError):
             p3ht_fresnel.set_type_color("ca", (0.1, 0.1, 0.1))
@@ -64,6 +75,12 @@ class TestFresnelGSD(BaseTest):
     def test_set_bad_unwrap_pos(self, p3ht_fresnel):
         with pytest.raises(ValueError):
             p3ht_fresnel.unwrap_positions = "true"
+
+    def set_set_colors(self, p3ht_fresnel):
+        colors = np.zeros_like(p3ht_fresnel.positions)
+        colors[:] = np.array([0.5, 0.5, 0.5])
+        p3ht_fresnel.colors = colors
+        assert all(np.array_equal(colors, p3ht_fresnel.colors))
 
     def test_unwrap_positions(self, p3ht_fresnel):
         assert p3ht_fresnel.unwrap_positions is False
