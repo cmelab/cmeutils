Fix histogram normalization (#78)
* Fix histogram normalization

* get rid of un-needed if/else statements

* Fix tests

cmeutils/plotting.py
@@ -29,9 +29,9 @@ def get_histogram(data, normalize=False, bins="auto", x_range=None):
         Array of the bin height values
 
     """
-    bin_heights, bin_borders = np.histogram(data, bins=bins, range=x_range)
-    if normalize is True:
-        bin_heights = bin_heights / sum(bin_heights)
+    bin_heights, bin_borders = np.histogram(
+        a=data, bins=bins, range=x_range, density=normalize
+    )
     bin_widths = np.diff(bin_borders)
     bin_centers = bin_borders[:-1] + bin_widths / 2
     return bin_centers, bin_heights

cmeutils/tests/test_structure.py
@@ -136,7 +136,8 @@ class TestStructure(BaseTest):
             histogram=True,
             normalize=True,
         )
-        assert np.allclose(np.sum(bonds_hist[:, 1]), 1, 1e-3)
+        bin_width = bonds_hist[:, 0][1] - bonds_hist[:, 0][0]
+        assert np.allclose(np.sum(bonds_hist[:, 1] * bin_width), 1, 1e-3)
 
     def test_bond_range_outside(self, p3ht_gsd):
         with pytest.warns(UserWarning):
@@ -172,7 +173,8 @@ class TestStructure(BaseTest):
             histogram=True,
             normalize=True,
         )
-        assert np.allclose(np.sum(angles_hist[:, 1]), 1, 1e-3)
+        bin_width = angles_hist[:, 0][1] - angles_hist[:, 0][0]
+        assert np.allclose(np.sum(angles_hist[:, 1] * bin_width), 1, 1e-3)
 
     def test_angle_range_outside(self, p3ht_gsd):
         with pytest.warns(UserWarning):